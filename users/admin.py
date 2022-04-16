8efcff30c5ddff32c16b32cb9f0973138d26de8d
brightmorkli37 <brightmorkli37@gmail.com>
2022-04-16 21:29:13 +0000

    add messages model

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Profile, Skill
+from .models import Profile, Skill, Message
 
 admin.site.register(Profile)
 admin.site.register(Skill)
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['subject', 'is_read', 'created']
+    filter_by = ['created']
 
