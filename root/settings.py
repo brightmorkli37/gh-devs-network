a8dd2eb387f297c65bb59182f26f90011cca0804
brightmorkli37 <brightmorkli37@gmail.com>
2022-10-12 07:08:30 +0000

    savepoint for tweaks

--- root/settings.py
+++ root/settings.py
@@ -123,9 +123,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
 EMAIL_HOST_USER = 'cosmicmirror0001@gmail.com'
-EMAIL_HOST_PASSWORD = '<4devWRKS/>'
+EMAIL_HOST_PASSWORD = 'eqddruxeptotyfwd'
 DEFAULT_FROM_EMAIL = "GH Developers Network <cosmicmirror0001@gmail.com>"
 
 
